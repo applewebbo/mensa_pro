100aae13145c45beba29b35ea79425033cd05887
webbo <e.bonardi@me.com>
2023-08-20 14:43:25 +0200

    tweaked account tests

--- accounts/tests/factories.py
+++ accounts/tests/factories.py
@@ -4,3 +4,5 @@ import factory
 class CustomUserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "accounts.CustomUser"
+
+    email = factory.Sequence(lambda n: "user_%d@test.com" % n)
--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -8,7 +8,7 @@ class TestCustomUser:
     def test_factory(self):
         """The factory produces a valid user instance"""
 
-        user = CustomUserFactory(email="test@test.com")
+        user = CustomUserFactory()
 
         assert user is not None
-        assert user.__str__() == "test@test.com"
+        assert user.__str__() == "user_0@test.com"
