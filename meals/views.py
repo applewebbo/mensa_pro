e7b45c9b38d34a4a2f9498a085c66f6b6f5d3ab3
webbo <e.bonardi@me.com>
2023-08-30 15:37:56 +0200

    modified profile for showing plus button to add meals for a week not present for #13

--- meals/views.py
+++ meals/views.py
@@ -23,11 +23,15 @@ def get_meals_per_week(menu, week_number):
     return menu.meals.filter(week=week_number)
 
 
+def get_weeks_choices():
+    return [number for number, name in Meal._meta.get_field("week").choices]
+
+
 @login_required
 def menu_view(request, pk):
     queryset = Menu.objects.prefetch_related("meals")
     menu = get_object_or_404(queryset, pk=pk)
-    weeks = [number for number, name in Meal._meta.get_field("week").choices]
+    weeks = get_weeks_choices()
     context = {
         "menu": menu,
         "weeks": weeks,
@@ -40,18 +44,25 @@ def menu_view(request, pk):
 @login_required
 def single_menu_with_active_weeks(request, pk):
     menu = Menu.objects.prefetch_related("meals").get(pk=pk)
-    weeks_choices = [number for number, name in Meal._meta.get_field("week").choices]
-    context = {}
-    max(weeks_choices)
+    weeks_choices = get_weeks_choices()
     weeks = {}
     for week in weeks_choices:
         meals_present = menu.meals.filter(week=week).exists()
         weeks.update({week: meals_present})
-    print(weeks)
+        if not meals_present:
+            break
+    context = {"menu": menu}
     context["weeks"] = weeks
     return render(request, "meals/includes/active-weeks.html", context)
 
 
 @login_required
 def menu_weekly_view(request, pk, week):
-    pass
+    menu = Menu.objects.prefetch_related("meals").get(pk=pk)
+    meals_of_the_week = menu.meals.filter(week=week)
+    context = {
+        "menu": menu,
+        "weekly_meals": meals_of_the_week,
+        "week": week,
+    }
+    return render(request, "meals/weekly-menu-view.html", context)
